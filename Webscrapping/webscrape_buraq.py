7b2c5dfdc2d2a4baaad63fc69b49ebc9c641432d
Annusanwer873 <56820250+Annusanwer873@users.noreply.github.com>
2022-01-06 13:27:41 +0500

    new
    check

---
+++ Webscrapping/webscrape_buraq.py
@@ -0,0 +1,103 @@
+from bs4 import BeautifulSoup as soup
+import requests
+import pandas as pd
+import os
+IMG_FOLDER = os.path.join('static', 'images')
+CSV_FOLDER = os.path.join('static', 'CSVs')
+
+buraq_Scrapped=""
+
+class CleanCache:
+
+    def __init__(self, directory=None):
+        self.clean_path = directory
+
+        if os.listdir(self.clean_path) != list():
+
+            files = os.listdir(self.clean_path)
+            for fileName in files:
+                print(fileName)
+                os.remove(os.path.join(self.clean_path, fileName))
+        print("cleaned!")
+
+class DataCollection:
+    def __init__(self):
+        self.data = {
+		"Name": list(),
+		"Price": list(),
+        "Link-For-Product": list(),
+        }
+
+    def get_final_data(self, commentbox=None):
+
+
+       try:
+
+            productlist = commentbox.find_all('li',{"class": "item"})
+            for item in productlist:
+                for link in item.find_all('a',{"class": "product-link"}):
+                    self.data["Link-For-Product"].append(link['href'])
+
+       except:
+             self.data["Link-For-Product"].append('none')
+
+       try:
+
+            for item in productlist:
+                for name in item.find_all('a',{"class": "product-link"}):
+                    self.data["Name"].append(name['title'])
+
+       except:
+            self.data["Name"].append('none')
+
+       try:
+            hh=[]
+            for item in productlist:
+             for link2 in item.find_all('span', {"class": "price"}):
+                 hh.append(link2.get_text().strip())
+
+            for i, j in enumerate(hh):
+                front, mid, end = j.partition(' ')
+                self.data["Price"].append(front)
+
+       except:
+             self.data["Price"].append('0')
+
+
+    def get_main_HTML(self, base_URL=None, cat = None , search_string=None,cat2= None,cat3=None):
+        search_url = f"{base_URL}/product-category/{cat}/{search_string}/{cat2}/{cat3}"
+        r= requests.get(search_url)
+        return soup(r.content,'lxml')
+
+    def get_data_dict(self):
+        return self.data
+
+    def save_as_dataframe(self, dataframe, fileName=None):
+        csv_path = os.path.join(CSV_FOLDER, fileName)
+        fileExtension = '.csv'
+        final_path = f"{csv_path}{fileExtension}"
+
+        CleanCache(directory=CSV_FOLDER)
+
+        dataframe.to_csv(final_path, index=None)
+        print("File saved successfully!!")
+        return final_path
+    
+
+
+    def bur_Scraped(self,intent,search_string,cat,cat2,cat3):
+        
+        base_URL = 'https://buraqstore.com'
+        search_string = search_string.replace(" ", "-")
+        print('processing, Please wait')
+        get_data = DataCollection()
+        buraq_HTML = get_data.get_main_HTML(base_URL,cat,search_string,cat2,cat3)
+
+        get_data.get_final_data(buraq_HTML)
+
+        buraq_Scrapped = pd.DataFrame(get_data.get_data_dict())
+
+        buraq_Scrapped = buraq_Scrapped.head(15)
+
+        return buraq_Scrapped
+    
